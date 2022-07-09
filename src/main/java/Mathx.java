2c173ac946c9013594bf48779ba54f94e2c3d92f
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2022-07-09 19:40:11 +0530

    add

---
+++ src/main/java/Mathx.java
@@ -0,0 +1,6 @@
+public class Mathx {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/MathxTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MathxTest {
+
+
+    static Mathx mt;
+
+    @Before
+    public void create(){
+        mt = new Mathx();
+    }
+
+    @Test
+    public void addTest(){
+        assertEquals(20,mt.add(-10,20));
+    }
+
+//    @Test
+//    public void subTest(){
+//        assertEquals(-20,mt.sub(-10,10));
+//    }
+//
+//    @Test
+//    public void multTest(){
+//        assertEquals(100,mt.mul(-10,10));
+//    }
+//
+//    @Test
+//    public void divTest(){
+//        assertEquals(10,mt.div(100,10));
+//    }
+}
