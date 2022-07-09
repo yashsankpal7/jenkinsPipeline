39a46a5a3729e3f951b4a8bcedcff8728863a7d7
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2022-07-09 23:39:20 +0530

    s

--- src/main/java/Mathx.java
+++ src/main/java/Mathx.java
@@ -8,7 +8,7 @@ public class Mathx {
         return a-b;
     }
 
-    public int mul(int a,int b){
-        return a*b;
-    }
+//    public int mul(int a,int b){
+//        return a*b;
+//    }
 }
